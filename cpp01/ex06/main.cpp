7c1a221c3e792281ab208c2afea23aacdd6d00e3
yfoucade <yfoucade@student.42.fr>
2022-11-04 13:26:49 +0100

    fix argument check

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yfoucade <yfoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 11:17:55 by yfoucade          #+#    #+#             */
-/*   Updated: 2022/11/02 13:41:09 by yfoucade         ###   ########.fr       */
+/*   Updated: 2022/11/04 13:22:16 by yfoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,13 @@ int	main(int ac, char **av)
 	if (ac < 2)
 	{
 		std::cout << "Usage: harlFilter min_level [levels...]" << std::endl;
-		exit(EXIT_FAILURE);
+		return (1);
 	}
-	if (std::string("DEBUG  INFO   WARNINGERROR  ").find(av[1]) == -1)
+	if (std::string("DEBUG") != av[1] && std::string("INFO") != av[1]
+		&& std::string("WARNING") != av[1] && std::string("ERROR") != av[1])
 	{
 		std::cout << "Invalid min_level argument: use one of DEBUG, INFO, WARNING, and ERROR" << std::endl;
-		exit(EXIT_FAILURE);
+		return (1);
 	}
 	for (int i = 2; i < ac; i++)
 		harl.complain(av[1], av[i]);
